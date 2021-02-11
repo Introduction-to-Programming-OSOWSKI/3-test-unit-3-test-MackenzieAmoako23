901f831aeeb40d2f5438552b356145e7206a8659
MackenzieAmoako23 <831590@mystma.org>
2021-02-11 17:29:03 +0000

    unit 3 test -1

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-#WRITE YOUR CODE IN THIS FILE
+def countB(w):
+    count = 0
+    for i in range(0, len(w)):
+        if w[i] == "b":
+            count = count + 1
+
+    return count
+
+def removeLast(w):
+    remove = ""
+    for i in range(0, len(w) - 1):
+        remove = remove + w[i]
+    return remove
+
+def sumBetweenOdd(x, y):
+    Villager = 0
+    for i in range(x + 1, y):
+        if   i % 2 == 1:
+            Villager = Villager + i 
+    return Villager
+
+def firstLast(w):
+    if w[0] == w[len(w) - 1]:
+        return True
+    else:
+        return False
